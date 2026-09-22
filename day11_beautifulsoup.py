3ac4dc7761a65b48beb7de7c043508f46f469b73
Muhammad Zohaib <aiengineering00@gmail.com>
2026-09-22 18:31:06 +0500

    day 11 use of BeautifulSoup HTML

--- day11_beautifulsoup.py
+++ day11_beautifulsoup.py
@@ -1,20 +1,32 @@
 from bs4 import BeautifulSoup
 html = """
-<html>
-    <div>
-        <h1>My Skills</h1>
-        <p>Python</p>
-        <p>Pandas</p>
-        <p>APIs</p>
-        <p>Web Scraping</p>
+    <div class="product">
+        <h2>Laptop</h2>
+        <p class="price">$800</p>
     </div>
-</html>
-"""
-soup = BeautifulSoup(html,"html.parser")
-print(soup.h1.text)
-print(soup.find_all("p"))
 
-for skill in soup.find_all("p"):
-    print(skill.text)
+    <div class="product">
+        <h2>Keyboard</h2>
+        <p class="price">$50</p>
+    </div>
+
+    <div class="product">
+        <h2>Mouse</h2>
+        <p class="price">$25</p>
+    </div>
+    """
+soup = BeautifulSoup(html,"html.parser")
+
+
+
+# 2. Saare product divs find_all() se extract karna
+products = soup.find_all("div", class_="product")    
+for product in products:
+
+    title = product.h2.text
+    price = product.find("p", class_="price").text
+    
+    # Required output format mein print karna
+    print(f"{title} - {price}")
 
 
